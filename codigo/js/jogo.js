89714c3a583ba6755c523aaa91e730fca131ffae
CamilleI <camille.goncalves@sga.pucminas.br>
2022-12-06 09:18:13 -0300

    add inicio do jogo

---
+++ codigo/js/jogo.js
@@ -0,0 +1,13 @@
+function pergunta(qual){
+    document.getElementById("mensagem").innerHTML = qual;
+}
+
+function yes() {
+    pergunta("Seu país faz fronteira com o Brasil?");
+    
+}
+
+
+function no() {
+    pergunta("Seu país faz fronteira com o Brasil?");
+}
